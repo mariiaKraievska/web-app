146fa8bd77a203767cfea0c5c2f2ffa9f68271a5
mariiaKraievska <105638281+mariiaKraievska@users.noreply.github.com>
2026-08-25 06:54:45 +0000

    Deploying to gh-pages from @ mariiaKraievska/web-app@9dc583764f6ea46bc921180c5557ec8bcf899731 🚀

--- chunk-EFBXUG3K.js
+++
@@ -1,1 +0,0 @@
-import{Qa as r}from"./chunk-YIE3LOP4.js";import{W as c,_ as e}from"./chunk-MPNEOCOO.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getGlAccounts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
